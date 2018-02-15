bfc031e954c6ddbd662f4b5600eb5652116753d0
Martin Weltler <weltlerm@gmail.com>
2018-02-15 15:51:39 +0100

    create(day4):Do the basic infos

---
+++ week-01/day-4/Exercises/src/DefineBasicInfo.java
@@ -0,0 +1,17 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myname = "Martin Weltler";
+        System.out.println(myname);
+        // Your age as an integer
+        int myage = 27;
+        System.out.println(myage);
+        // Your height in meters as a double
+        double myheight = 1.82;
+        System.out.println(myheight);
+        // Whether you are married or not as a boolean
+        boolean married = (false);
+        System.out.println("Married?: " + married);
+    }
+}
