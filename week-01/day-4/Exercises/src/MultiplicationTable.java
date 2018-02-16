1c924160e5aeea267967250806f103215af6b308
Martin Weltler <weltlerm@gmail.com>
2018-02-16 12:17:50 +0100

    create(day5):Do multiplicationTable task

---
+++ week-01/day-4/Exercises/src/MultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int x = 0;
+        while (x < 10) {
+            System.out.println("Give me a whole number:");
+            int userInput1 = scanner.nextInt();
+            for (int i = 1; i < 11; i++) {
+                System.out.println(i + " * " + userInput1 + " = " + i*userInput1 );
+            }
+        }
+    }
+    // Create a program that asks for a number and prints the multiplication table with that number
+}
