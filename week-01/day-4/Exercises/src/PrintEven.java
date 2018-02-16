e4f4c589011d132a6006670b375c4a2593545844
Martin Weltler <weltlerm@gmail.com>
2018-02-16 12:02:16 +0100

    create(day5):Do printeven task

---
+++ week-01/day-4/Exercises/src/PrintEven.java
@@ -0,0 +1,12 @@
+public class PrintEven {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 500) {
+            if (a%2 == 0) {
+                System.out.println(a);
+            }
+            a++;
+        }
+    }
+    // Create a program that prints all the even numbers between 0 and 500
+}
