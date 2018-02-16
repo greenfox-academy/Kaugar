2dd20ddbf46f0fd2cc59bfc6a8bfd2ab04aa93bc
Martin Weltler <weltlerm@gmail.com>
2018-02-16 11:58:01 +0100

    create(day5):Do wontcheat task

---
+++ week-01/day-4/Exercises/src/IWontCheatOnExams.java
@@ -0,0 +1,11 @@
+public class IWontCheatOnExams {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 100) {
+            System.out.println("I wont cheat on the exam!");
+            a++;
+        }
+    }
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+}
