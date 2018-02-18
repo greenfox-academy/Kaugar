50481c4dc452c1a62d9b81c97c8a60f0bf70aeb8
Martin Weltler <weltlerm@gmail.com>
2018-02-18 15:40:06 +0100

    feat(day5)crate DrawDiagonal task

--- week-01/day-4/Exercises/src/DrawDiagonal.java
+++ week-01/day-4/Exercises/src/DrawDiagonal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DrawSquare {
+public class DrawDiagonal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give me a number");
@@ -10,31 +10,30 @@ public class DrawSquare {
             if (i == 0 || i == userInput1-1) {
                 for (int j = 0; j < userInput1; j++)
                     System.out.print("%");
-            }
-            else {
+            } else {
                 System.out.print("%");
-                for (int j = 1; j < userInput1-1; j++) {
+                for (int k = 0; k < i-1; k++) {
+                        System.out.print(" ");
+                    }
+                System.out.print("%");
+                for (int l = userInput1-2-i; l > 0; l--) {
                     System.out.print(" ");
                 }
                 System.out.print("%");
-
             }
-
             System.out.println("");
         }
     }
 }
-
 
 // Write a program that reads a number from the standard input, then draws a
 // square like this:
 //
 //
 // %%%%%
-// %   %
-// %   %
-// %   %
-// %   %
+// %%  %
+// % % %
+// %  %%
 // %%%%%
 //
 // The square should have as many lines as the number was
