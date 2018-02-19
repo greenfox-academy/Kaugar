8847acf845f5f261a83b8b937f90140dfc0cc7b2
Martin Weltler <weltlerm@gmail.com>
2018-02-19 16:01:11 +0100

    feat(w-02,d1)do PrintAll exercise

---
+++ week-02/day1/Exercises/src/PrintAll.java
@@ -0,0 +1,12 @@
+public class PrintAll {
+    public static void main(String[] args){
+        int[] af = {4,5,6,7};
+        for (int i = 0; i < af.length; i++) {
+            System.out.println(af[i]);
+        }
+
+    }
+}
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
