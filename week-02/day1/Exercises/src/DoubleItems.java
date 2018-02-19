14d594a7e828e3b7378d86765ddbaadd47269999
Martin Weltler <weltlerm@gmail.com>
2018-02-19 16:33:54 +0100

    feat(w-02,d1)do DoubleItems exercise

---
+++ week-02/day1/Exercises/src/DoubleItems.java
@@ -0,0 +1,14 @@
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] ag = {3, 4, 5, 6, 7};
+        for (int i = 0; i < ag.length; i++) {
+            ag[i] = ag[i] * 2;
+            }
+        for (int j = 0; j < ag.length; j++) {
+            System.out.println(ag[j]);
+        }
+    }
+}
+// - Create an array variable named `ag`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
