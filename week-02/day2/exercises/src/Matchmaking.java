df3c4a1d234086fc3d2fcc398af71ab1c5c399a7
Martin Weltler <weltlerm@gmail.com>
2018-02-20 15:31:21 +0100

    feat(w-02,d2)Do MatchMaking task

---
+++ week-02/day2/exercises/src/Matchmaking.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Matchmaking{
+    public static void main(String... args){
+        ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
+        ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
+
+        // Write a method that joins the two lists by matching one girl with one boy into a new list
+        // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+        System.out.println(makingMatches(girls, boys));
+    }
+    public static ArrayList makingMatches (ArrayList<String> women, ArrayList <String> men){
+
+        for (int i = 0; i < men.size(); i++) {
+            women.add(i, men.get(i));
+
+        }
+        return women;
+    }
+}
