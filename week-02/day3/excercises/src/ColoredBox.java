a6d9727e0eac6c7f9875d31120c1be4250fc3b5e
Martin Weltler <weltlerm@gmail.com>
2018-02-21 10:55:29 +0100

    feat(w-02,d3)Do ColoredBox task

---
+++ week-02/day3/excercises/src/ColoredBox.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ColoredBox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+        graphics.setColor(Color.RED);
+        graphics.drawLine(20,20,100,20);
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(100,20,100,80);
+        graphics.setColor(Color.BLUE);
+        graphics.drawLine(100,80,20,80);
+        graphics.setColor(Color.YELLOW);
+        graphics.drawLine(20,80,20,20);
+
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
