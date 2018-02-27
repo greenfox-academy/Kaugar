db1a541f943d45a636183844fad18a42e12f24cb
Martin Weltler <weltlerm@gmail.com>
2018-02-27 23:20:44 +0100

    feat(week03,day2) Complete the Logs exercise

--- week-03/day2/File-manipulation/src/Logs.java
+++ week-03/day2/File-manipulation/src/Logs.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,23 +9,44 @@ import java.util.List;
 
 public class Logs {
   public static void main(String[] args) {
+    System.out.println(unique("log.txt"));
+    System.out.println(ratio("log.txt"));
 
 
   }
-  public static void unique (List<String> adresses){
-    String uniquestring = "";
+  public static ArrayList<String> unique (String adresses){
+    ArrayList<String> uniqips = new ArrayList<>();
     try {
-      Path path1 = Paths.get("log.txt");
+      Path path1 = Paths.get(adresses);
       List<String> lines = Files.readAllLines(path1);
-      for (int i = 0; i < lines.size() ; i++) {
-        lines.add(uniquestring);
+      for (int i = 0; i < lines.size(); i++) {
+        if(!uniqips.contains(lines.get(i).substring(27,38)))
+        uniqips.add(lines.get(i).substring(27,38));
       }
-
     } catch (IOException e){
       System.out.println("Cant Read");
     }
-    System.out.println(uniquestring);
-
+    return uniqips;
+  }
+  public static String ratio (String getandpost){
+    int gets = 0;
+    int posts = 0;
+    String answer = "";
+    try{
+      Path path1 = Paths.get(getandpost);
+      List<String> lines = Files.readAllLines(path1);
+      for (int i = 0; i < lines.size(); i++) {
+        if (lines.get(i).contains("GET")){
+          gets++;
+        }else {
+          posts++;
+        }
+      }
+    } catch (IOException e){
+      System.out.println("Cant Read");
+    }
+    answer = "Ratio(GET / POST) : " + Integer.toString(gets) + " / " + Integer.toString(posts);
+    return answer;
   }
 }
 
