002226354531f33d11015efc64c3f8c3b4eca24c
Martin Weltler <weltlerm@gmail.com>
2018-02-28 12:13:06 +0100

    feat(week03,day3)do PostIt,BlogPost exercises

--- week-03/day3/src/PostIt.java
+++ week-03/day3/src/PostIt.java
@@ -1,11 +1,11 @@
 public class PostIt {
   String backgroundColor;
   String text;
-  String textcolor;
-  public PostIt (){
-    PostIt Idea1 = new PostIt();
-    PostIt Awesome = new PostIt();
-    PostIt Superb = new PostIt();
+  String textColor;
+  public PostIt (String backgroundColor, String text, String textColor){
+    PostIt postit1 = new PostIt(backgroundColor="orange", text="Idea1", textColor="blue");
+    PostIt postit2 = new PostIt("pink","Awesome","black");
+    PostIt postit3 = new PostIt("yellow", "Superb!", "green");
   }
 }
 
