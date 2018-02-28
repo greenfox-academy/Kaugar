a094af7d28a19a0db764093863ce786d32375b1b
Martin Weltler <weltlerm@gmail.com>
2018-02-28 11:50:05 +0100

    feat(week03,day3)Create directories

---
+++ week-03/day3/src/PostIt.java
@@ -0,0 +1,11 @@
+public class PostIt {
+  String backgroundColor;
+  String text;
+  String textcolor;
+  public PostIt (){
+    PostIt Idea1 = new PostIt();
+    PostIt Awesome = new PostIt();
+    PostIt Superb = new PostIt();
+  }
+}
+
