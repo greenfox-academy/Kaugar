2cc87eed93b864a0382f858c7718d8d6bada8acd
Martin Weltler <weltlerm@gmail.com>
2018-03-01 13:08:48 +0100

    feat(week03,day4)Do String2 task

--- week-03/day4/src/String2.java
+++ week-03/day4/src/String2.java
@@ -1,2 +1,22 @@
 public class String2 {
+  public static void main(String[] args) {
+
+    String newString = "Hello x I am x and x";
+    System.out.println(charChanger(newString));
+
+  }
+  public static String charChanger(String word) {
+    char temp;
+    if (word.length() == 0) {
+      return word;
+    } else {
+      if ('x' == word.charAt(0)) {
+        temp = 0;
+      } else {
+        temp = word.charAt(0);
+      }
+      return temp + charChanger(word.substring(1));
+    }
+  }
 }
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
