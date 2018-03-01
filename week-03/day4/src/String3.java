ee59bf3692fb477d214f8d41e2e86705680871e7
Martin Weltler <weltlerm@gmail.com>
2018-03-01 14:45:09 +0100

    feat(week03,day4)Do String3 task

--- week-03/day4/src/String3.java
+++ week-03/day4/src/String3.java
@@ -1,5 +1,17 @@
 public class String3 {
   public static void main(String[] args) {
-    
+
+    String newString = "Hello x I am x and x";
+    System.out.println(charChanger(newString));
+
+  }
+  public static String charChanger(String word) {
+    if (word.length() == 0) {
+      return word;
+    } else {
+      return word.charAt(0) + "*" + charChanger(word.substring(1));
+    }
   }
 }
+// Given a string, compute recursively a new string where all the
+// adjacent chars are now separated by a "*".
