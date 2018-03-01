510870cd73e7ac1a3800266c23709ed54db9e3d4
Martin Weltler <weltlerm@gmail.com>
2018-03-01 12:56:53 +0100

    feat(week03,day4)Do String1 task

--- week-03/day4/src/String1.java
+++ week-03/day4/src/String1.java
@@ -1,14 +1,26 @@
 public class String1 {
   public static void main(String[] args) {
 
-  }
-  public static String charChanger (String word){
-    
-    if (n == 1){
+    String test = "Mx name is xIx";
+    System.out.println(charChanger(test));
 
+
+  }
+
+  public static String charChanger(String word) {
+    char temp;
+    if (word.length() == 0) {
+      return word;
+    } else {
+      if ('x' == word.charAt(0)) {
+        temp = 'y';
+      } else {
+        temp = word.charAt(0);
+      }
+      return temp + charChanger(word.substring(1));
     }
-
   }
+
 }
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
