8117f43d8f42a277c8afb50d6f40ab69184db067
Martin Weltler <weltlerm@gmail.com>
2018-03-06 14:49:57 +0100

    feat(Garden)create Garden and Flower classes

--- week-04/day2/src/GardenApp/Garden.java
+++ week-04/day2/src/GardenApp/Garden.java
@@ -1,5 +1,48 @@
 package GardenApp;
 
-public class Garden {
+import java.util.ArrayList;
 
+public class Garden {
+  ArrayList<Plant> plants;
+
+  public Garden () {
+    this.plants = new ArrayList<>();
+  }
+  public void addTree (Plant.Tree tree){
+    plants.add(tree);
+  }
+  public void addFlower (Plant.Flower flower){
+    plants.add(flower);
+  }
+  public void watering (double waterToGarden){
+
+    System.out.println("\n" + "Watering with " + waterToGarden);
+
+    double waterToPlants = waterToGarden/plants.size();
+    for (int i = 0; i < plants.size() ; i++) {
+      plants.get(i).watering(waterToPlants);
+    }
+    for (int i = 0; i < plants.size() ; i++) {
+      plants.get(i).needswater();
+    }
+  }
+
+  public static void main(String[] args) {
+    Garden garden1 = new Garden();
+
+    Plant.Flower flower1 = new Plant.Flower("yellow");
+    garden1.addFlower(flower1);
+    Plant.Flower flower2 = new Plant.Flower("blue");
+    garden1.addFlower(flower2);
+    Plant.Tree tree1 = new Plant.Tree("purple");
+    garden1.addTree(tree1);
+    Plant.Tree tree2 = new Plant.Tree("orange");
+    garden1.addTree(tree2);
+
+    garden1.watering(40);
+    garden1.watering(70);
+
+
+
+  }
 }
---
+++ week-04/day2/src/GardenApp/Plant.java
@@ -0,0 +1,40 @@
+package GardenApp;
+
+public class Plant {
+  String type;
+  double water = 0;
+  String color;
+  int needOfWater;
+  double absorbing;
+
+  public void needswater() {
+    if (water < needOfWater) {
+      System.out.println("The " + color + " " + type + " needs water");
+    }else {
+      System.out.println("The " + color + " " + type + " doesn't need water");
+    }
+  }
+  public void watering (double water){
+    this.water += absorbing * water;
+  }
+
+  public static class Tree extends Plant{
+
+    public Tree(String color) {
+      super.type = "Tree";
+      super.color = color;
+      super.needOfWater = 10;
+      super.absorbing = 0.4;
+    }
+  }
+
+  public static class Flower extends Plant{
+
+    public Flower(String color) {
+      super.type = "Flower";
+      super.color = color;
+      super.needOfWater = 5;
+      super.absorbing = 0.75;
+    }
+  }
+}
