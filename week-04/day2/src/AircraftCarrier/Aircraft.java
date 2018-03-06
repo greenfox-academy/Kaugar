c8fedca866b9a5f8f9bf3fec5c7a462fed344328
Martin Weltler <weltlerm@gmail.com>
2018-03-06 16:18:14 +0100

    feat(AirCraft)add methods to Aircrafts

--- week-04/day2/src/AircraftCarrier/Aircraft.java
+++ week-04/day2/src/AircraftCarrier/Aircraft.java
@@ -4,15 +4,29 @@ public class Aircraft {
   int ammo = 0;
   int maxAmmo;
   int baseDamage;
+  String type;
 
   public int fight (){
     int damage = this.ammo * this.baseDamage;
     ammo = 0;
     return damage;
   }
-  public void refill (){
+  public int fill (int remaining){
+    int ammoNeeded = this.maxAmmo - this.ammo;
+    int filledAmmo = 0;
+    for (int i = 0; i < ammoNeeded; i++) {
+      this.ammo++;
+      filledAmmo++;
+    }
+    remaining -= filledAmmo;
+    return remaining;
+  }
 
-
+  public String getType() {
+    return type;
+  }
+  public void getStatus () {
+    System.out.println("Type: " + type + ", Ammo: " + ammo + ", Base Damage: " + baseDamage + ", All Damage: " + fight());
   }
   public static class F16 extends Aircraft {
     public F16 (){
---
+++ week-04/day2/src/AircraftCarrier/Carrier.java
@@ -0,0 +1,4 @@
+package AircraftCarrier;
+
+public class Carrier {
+}
