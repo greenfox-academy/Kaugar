f3ec1873c0fcffc7f38e8212abe7d26ccf2d7701
Martin Weltler <weltlerm@gmail.com>
2018-03-07 13:49:41 +0100

    feat(Domino)Do the task

---
+++ week-04/Interfaces/src/Domino.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Domino implements Comparable<Domino> {
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+
+  public static void main(String[] args) {
+
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+
+    Collections.sort(dominoes);
+    System.out.println(dominoes);
+
+  }
+
+  @Override
+  public int compareTo(Domino domino){
+    if(values[0] == domino.values[0])
+      return 0;
+    else if(values[0]>domino.values[0])
+      return 1;
+    else
+      return -1;
+  }
+}
