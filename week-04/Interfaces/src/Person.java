599967e37325333e36ef20ec4a5b1d7ff355c5d4
Martin Weltler <weltlerm@gmail.com>
2018-03-07 12:16:32 +0100

    feat(week04,day4)Create directory

---
+++ week-04/Interfaces/src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+  protected String name;
+  protected int age;
+  protected String gender;
+
+  public void introduce (){
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + ".");
+  }
+  public void getGoal (){
+    System.out.println("My goal is: Live for the moment!");
+  }
+  public Person(String name, int age, String gender){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  public Person(){
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+}
---
+++ week-04/Interfaces/src/Student.java
@@ -0,0 +1,39 @@
+public class Student extends Person implements Cloneable {
+  String previousOrganization;
+  int skippedDays;
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old "
+            + gender + " from " + previousOrganization + " who skipped "
+            + skippedDays + " days from the course already.");
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Be a junior software developer!");
+  }
+
+  public void skipDays(int numberOfdays) {
+    this.skippedDays += numberOfdays;
+  }
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name,age,gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public Student() {
+    super();
+    this.previousOrganization = "The School of Life";
+  }
+
+  public static void main(String[] args) throws CloneNotSupportedException {
+
+    Student john = new Student("John", 20,"male", "BME");
+    Student john2 = (Student)john.clone();
+
+    john2.introduce();
+  }
+}
