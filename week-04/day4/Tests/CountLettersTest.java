75b930e28bef6c361c14ae7293e1bac71a84ce8a
Martin Weltler <weltlerm@gmail.com>
2018-03-08 16:59:31 +0100

    feat(Countletters) Do the tests

--- week-04/day4/Tests/CountLettersTest.java
+++ week-04/day4/Tests/CountLettersTest.java
@@ -1,22 +1,31 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 public class CountLettersTest {
 
-  CountLetters word = 
+  CountLetters test;
+
 
   @Before
   public void setUp() throws Exception {
+    test = new CountLetters();
 
   }
 
   @Test
-  public void counLetters() {
-  }
+  public void countLetters() {
+    String word = "Hello";
 
-  @Test
-  public void main() {
+    HashMap<Character,Integer> testmap = new HashMap<>();
+    testmap.put('H', 1);
+    testmap.put('e', 1);
+    testmap.put('l', 2);
+    testmap.put('o', 1);
+
+    assertEquals(testmap, test.countLetters(word));
   }
 }
--- week-04/day4/src/CountLetters.java
+++ week-04/day4/src/CountLetters.java
@@ -2,22 +2,18 @@ import java.util.HashMap;
 
 public class CountLetters {
 
-  public static HashMap counLetters (String word){
+
+  public static HashMap countLetters(String word) {
 
     HashMap<Character, Integer> hmap = new HashMap<>();
 
     for (int i = 0; i < word.length(); i++) {
-      if (hmap.containsKey(word.charAt(i))){
-        hmap.put(word.charAt(i), hmap.get(word.charAt(i))+1);
+      if (hmap.containsKey(word.charAt(i))) {
+        hmap.put(word.charAt(i), hmap.get(word.charAt(i)) + 1);
       } else {
         hmap.put(word.charAt(i), 1);
       }
     }
     return hmap;
-  }
-
-  public static void main(String[] args) {
-
-    System.out.println(counLetters("Hello"));
   }
 }
