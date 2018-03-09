7d43e439066add400d30f76515eb9c716720ba61
Martin Weltler <weltlerm@gmail.com>
2018-03-09 15:36:43 +0100

    fix(Reservations) fix the generating method

--- week-04/day3/Interfaces/src/Reservations/Reservations.java
+++ week-04/day3/Interfaces/src/Reservations/Reservations.java
@@ -1,23 +1,26 @@
 package Reservations;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Reservations implements Reservatory {
   private String day;
   private String code;
 
+
+
   public Reservations(){
     day = getDowBooking();
-    code = getDowBooking();
+    code = getCodeBooking();
   }
   @Override
   public String getDowBooking() {
 
     String[] days = {"MON", "THU", "WED", "THU", "FRI", "SAT", "SUN"};
     Random ran = new Random();
-    int randnum = ran.nextInt(days.length);
-    String randomday = days[randnum];
-    return randomday;
+    int randNum = ran.nextInt(days.length);
+    String randomDay = days[randNum];
+    return randomDay;
   }
 
   @Override
@@ -27,15 +30,20 @@ public class Reservations implements Reservatory {
     String codestring = "";
     for (int i = 0; i < 8 ; i++) {
       Random ran = new Random();
-      int randnum = ran.nextInt(alphanum.length);
-      codestring += String.valueOf(alphanum[randnum]);
+      int randNum = ran.nextInt(alphanum.length);
+      codestring += String.valueOf(alphanum[randNum]);
     }
     return codestring;
   }
 
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Bookings to generate:");
+    Integer input = scanner.nextInt();
+
     Reservations reservation = new Reservations();
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < input; i++) {
       System.out.println("Booking# " + reservation.getCodeBooking() + " for " + reservation.getDowBooking());
     }
   }
---
+++ week-04/day3/Interfaces/src/Reservations/Try.java
@@ -0,0 +1,13 @@
+package Reservations;
+
+public class Try implements Reservatory {
+  @Override
+  public String getDowBooking() {
+    return null;
+  }
+
+  @Override
+  public String getCodeBooking() {
+    return null;
+  }
+}
