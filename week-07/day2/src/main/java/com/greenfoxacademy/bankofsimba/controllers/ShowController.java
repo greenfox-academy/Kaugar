5645ea2444e8ecd2580a09500c07440fa3b57529
Martin Weltler <weltlerm@gmail.com>
2018-04-04 15:09:39 +0200

    feat(day2)add models and controllers

--- week-07/day2/src/main/java/com/greenfoxacademy/bankofsimba/controllers/ShowController.java
+++ week-07/day2/src/main/java/com/greenfoxacademy/bankofsimba/controllers/ShowController.java
@@ -1,14 +1,18 @@
 package com.greenfoxacademy.bankofsimba.controllers;
 
 import com.greenfoxacademy.bankofsimba.models.BankAccount;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShowController {
 
+
   @RequestMapping (value = "/show")
-  public BankAccount bankAccount (){
-    return new BankAccount("Simba", 2000, "lion");
+  public String bankAccount (Model model){
+    model.addAttribute(new BankAccount("Simba", 2000, "lion"));
+    return "bankaccountvalues";
+
   }
 }
--- week-07/day2/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
+++ week-07/day2/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
@@ -1,13 +1,25 @@
 package com.greenfoxacademy.bankofsimba.models;
 
 public class BankAccount {
-  String name;
-  double balance;
-  String animalType;
+  private String name;
+  private double balance;
+  private String animalType;
 
   public BankAccount(String name, double balance, String animalType) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
   }
 }
