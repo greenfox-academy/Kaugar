a63dcffdc0adb8008b744a0e3dc6b69222c62d18
Martin Weltler <weltlerm@gmail.com>
2018-04-05 20:19:37 +0200

    feat(HelloBeanWorld)create emailchecker

--- week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/controllers/Controller.java
+++ week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/controllers/Controller.java
@@ -4,12 +4,15 @@ import com.greenfoxacademy.hellobeanworld.services.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
   @Autowired
   UtilityService utilityService;
+
 
   @GetMapping(value = "/useful")
   public String link (Model model){
@@ -19,9 +22,17 @@ public class Controller {
   }
 
   @GetMapping(value = "/useful/colored")
-  public String pagewithrandombackground (Model model){
+  public String pageWithRandomBackground (Model model){
     model.addAttribute("randomcolor", utilityService.randomColor());
     return "colored";
-
   }
+
+  @GetMapping(value="/useful/email")
+  public String email (Model model, @RequestParam(name = "email") String emailToCheck) {
+    model.addAttribute("isValid", utilityService.validateEmail(emailToCheck));
+    model.addAttribute("email", emailToCheck);
+    return "email";
+  }
+
+
 }
--- week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/services/UtilityService.java
+++ week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/services/UtilityService.java
@@ -23,4 +23,16 @@ public class UtilityService {
   public String randomColor() {
     return colors.get(random.nextInt(colors.size()));
   }
+
+  public boolean validateEmail(String email) {
+    return (email.contains("@") && email.contains("."));
+  }
+
+  public String caesar(String text, int number) {
+    String result = "";
+    for(int i = 0; i < text.length(); i++) {
+      result += (char)((int)text.charAt(i) + number);
+    }
+    return result;
+  }
 }
