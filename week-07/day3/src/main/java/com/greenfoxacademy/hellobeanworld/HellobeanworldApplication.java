805d6400061a3d4f48d823f2d7433bd459d6d136
Martin Weltler <weltlerm@gmail.com>
2018-04-05 11:35:24 +0200

    feat(SpellChecker) creating Spellchecker class

--- week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
+++ week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -1,12 +1,21 @@
 package com.greenfoxacademy.hellobeanworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellobeanworldApplication {
+public class HellobeanworldApplication implements CommandLineRunner {
+  @Autowired
+  SpellChecker checker;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellobeanworldApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		checker.checkSpelling();
 	}
 }
