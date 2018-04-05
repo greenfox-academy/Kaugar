76ae41f83a23a6067a7170240062e006737f6a6e
Martin Weltler <weltlerm@gmail.com>
2018-04-05 17:01:20 +0200

    feat(HelloBeanWorld)create color random utility

--- week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/controllers/Controller.java
+++ week-07/day3/src/main/java/com/greenfoxacademy/hellobeanworld/controllers/Controller.java
@@ -1,10 +1,15 @@
 package com.greenfoxacademy.hellobeanworld.controllers;
 
+import com.greenfoxacademy.hellobeanworld.services.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+  @Autowired
+  UtilityService utilityService;
 
   @GetMapping(value = "/useful")
   public String link (Model model){
@@ -14,8 +19,8 @@ public class Controller {
   }
 
   @GetMapping(value = "/useful/colored")
-  public String bankAccount (Model model){
-    model.addAttribute("colored", "hello");
+  public String pagewithrandombackground (Model model){
+    model.addAttribute("randomcolor", utilityService.randomColor());
     return "colored";
 
   }
