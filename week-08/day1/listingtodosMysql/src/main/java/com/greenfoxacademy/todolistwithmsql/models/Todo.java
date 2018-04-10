474856d8610b59e7ae926fb8ded56bd7ebbc2e72
Martin Weltler <weltlerm@gmail.com>
2018-04-10 12:39:59 +0200

    feat(toDoListMySQL)create directories

---
+++ week-08/day1/listingtodosMysql/src/main/java/com/greenfoxacademy/todolistwithmsql/models/Todo.java
@@ -0,0 +1,58 @@
+package com.greenfoxacademy.listingtodos.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Id
+  private long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+
+  public Todo(String title) {
+    this.title = title;
+    urgent = false;
+    done = false;
+  }
+
+  public Todo() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
